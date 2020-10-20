18726c59791728f5d278eb1d2f9b6fba9efa2d50
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-19 21:48:09 -0500

    Adding controllers and routes

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+
+const createUser = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'Create user!!!'
+    });
+};
+
+module.exports = {
+    createUser
+}
--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+//DB Config
+ const { dbConnection } = require('./database/config')
+ dbConnection();
+
 // App Express
 const app = express();
+
+//Body HTTP parser
+app.use( express.json() );
 
 //Node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-
+//Public Path
 const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ));
+
+//Routes
+app.use( '/api/login', require('./routes/auth') );
 
 server.listen( process.env.PORT, ( err ) => {
 
