99686cc58cc19c32c69cd9e74a8a0d912fda8c14
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-19 23:26:03 -0500

    Adding express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
+
+    const errors = validationResult( req );
+
+    if( !errors.isEmpty() ){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
     res.json({
         ok: true,
         msg: 'Create user!!!'
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 /*
     path: api/login
 */
-
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { createUser } = require('../controllers/auth');
+
 
 const router = Router();
 
-router.post('/new', createUser);
+router.post('/new', [
+    check('name', 'The name is required').not().isEmpty(),
+] , createUser);
 
 module.exports = router;
