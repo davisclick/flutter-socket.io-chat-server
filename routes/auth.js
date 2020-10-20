98db5ca357d141a38b2b310de6996b1b37675738
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-19 23:38:22 -0500

    Adding validations

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,15 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { createUser } = require('../controllers/auth');
-
+const { validatorFields } = require('../middlewares/validator-fields');
 
 const router = Router();
 
 router.post('/new', [
     check('name', 'The name is required').not().isEmpty(),
+    check('password', 'The password is required').not().isEmpty(),
+    check('email', 'The email is required').isEmail(),
+    validatorFields
 ] , createUser);
 
 module.exports = router;
