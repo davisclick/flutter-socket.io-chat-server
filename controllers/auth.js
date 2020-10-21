2f55ef1c822e247b82908733eb915226d55f6d80
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-20 19:12:54 -0500

    Avoid duplicate email

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,14 +5,36 @@ const User = require('../models/user');
 
 const createUser = async (req, res = response) => {
 
-    const user = new User( req.body );
+    const { email } = req.body;
 
-    await user.save();
+    try {
 
-    res.json({
-        ok: true,
-        body: req.body
-    });
+        const existEmail = await User.findOne({ email });
+
+        if(existEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'The email exist in the database'
+            });
+        }
+            
+        const user = new User( req.body );
+        await user.save();
+
+        res.json({
+            ok: true,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact the admin service'
+        });
+    }
+
 };
 
 module.exports = {
